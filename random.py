18f3dc156bf259a6bffa11090d4c94aa65a19e89
Jamshid <irisbayevjamshid@gmail.com>
2025-03-01 14:47:36 +0500

    app.py faylida o'zgarishlar qilindi

--- random.py
+++ random.py
@@ -5,22 +5,22 @@ def tahmin_oyini():
     tasodifiy_son = random.randint(1, max_son)
     urinishlar = 0
     
-    print("📢 Men 1 dan 100 gacha bir son o‘yladim. Uni topa olasanmi?")
+    print(" Men 1 dan 100 gacha bir son o‘yladim. Uni topa olasanmi?")
 
     while True:
         try:
-            taxmin = int(input("❓ Sonni kiriting: "))
+            taxmin = int(input(" Sonni kiriting: "))
             urinishlar += 1
             
             if taxmin < tasodifiy_son:
-                print("🔼 Men o‘ylagan son bundan kattaroq!")
+                print(" Men o‘ylagan son bundan kattaroq!")
             elif taxmin > tasodifiy_son:
-                print("🔽 Men o‘ylagan son bundan kichikroq!")
+                print(" Men o‘ylagan son bundan kichikroq!")
             else:
-                print(f"🎉 Tabriklayman! To‘g‘ri topding! ✅ ({urinishlar} urinishda)")
+                print(f" Tabriklayman! To‘g‘ri topding!  ({urinishlar} urinishda)")
                 break  
         except ValueError:
-            print("⚠ Iltimos, butun son kiriting!")
+            print(" Iltimos, butun son kiriting!")
 
 tahmin_oyini()
 
